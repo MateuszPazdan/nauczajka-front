5162e1fb52eb1f4be9a0abc0470543f5d98973f4
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-08-06 16:12:42 +0200

    error page reset delete from props

--- app/error.tsx
+++ app/error.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 
-export default function Error({ error, reset }: any) {
+export default function Error({ error }: any) {
 	return (
 		<main className='flex justify-center items-center flex-col gap-6 min-h-full text-center'>
 			<div className='flex gap-5 items-center flex-col w-[300px] '>
