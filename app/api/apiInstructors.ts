e41dc2297055313e38aa9be604f7d46a6b9a1819
Mateusz Pazdan <pazdan.mateuszz@gmail.com>
2024-08-06 16:11:53 +0200

    error Page

--- app/api/apiInstructors.ts
+++ app/api/apiInstructors.ts
@@ -74,7 +74,7 @@ export async function getAllSkillsAction() {
 		const data = await response.json();
 		return data;
 	} catch (error) {
-		throw new Error('Nie udało się pobrać listy umiejętności');
+		console.log('Nie udało się pobrać listy umiejętności');
 	}
 }
 
--- app/error.tsx
+++ app/error.tsx
@@ -1,17 +1,21 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function Error({ error, reset }: any) {
 	return (
-		<main className='flex justify-center items-center flex-col gap-6'>
-			<h1 className='text-3xl font-semibold'>Coś poszło nie tak</h1>
-			<p className='text-lg'>{error.message}</p>
-
-			<button
-				onClick={reset}
-				className='inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg'
+		<main className='flex justify-center items-center flex-col gap-6 min-h-full text-center'>
+			<div className='flex gap-5 items-center flex-col w-[300px] '>
+				<h1 className='text-5xl font-semibold'>Oops!</h1>
+				<p className='text-lg font-bold text-mainSalmon'>{error.message}</p>
+				<img src='/error.png' alt='Error image' className='w-[200px] ' />
+			</div>
+			<Link
+				href='/'
+				className='inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg text-white duration-300 transition-colors hover:bg-mainHover rounded-md bg-main'
 			>
-				Spróbuj ponownie
-			</button>
+				Wróć na stronę główną
+			</Link>
 		</main>
 	);
 }
